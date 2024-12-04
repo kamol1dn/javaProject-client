b246c8827021715ba12750e9aa1833ef14d35098
kamol1dn <kamoliddinsharopov1@gmail.com>
2024-12-04 16:07:24 +0500

    timetable

--- src/utils/ScreenUtils.java
+++ src/utils/ScreenUtils.java
@@ -5,8 +5,7 @@ public class ScreenUtils {
     public static final String RESET = "\u001B[0m";
 
     // Text Colors
-    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
+   public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
